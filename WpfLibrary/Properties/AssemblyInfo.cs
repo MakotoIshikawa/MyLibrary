0d6df8ec32e26676ef87aef8647d5d6501598fa9
NVTOKYO\ishikawam <ishikawam@nextvision.co.jp>
2018-06-27 15:50:24 +0900

    セレクターのバインド拡張メソッド追加

--- WpfLibrary/Properties/AssemblyInfo.cs
+++ WpfLibrary/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.8.6.*")]
-[assembly: AssemblyFileVersion("1.8.6.7")]
+[assembly: AssemblyFileVersion("1.8.6.27")]
