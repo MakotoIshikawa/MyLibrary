c8631ca70601b87eb7e34a042b518ed98384ef6d
NVTOKYO\ishikawam <ishikawam@nextvision.co.jp>
2018-06-27 14:50:56 +0900

    Excel管理クラス実装

--- OfficeLibrary/ExcelManager.cs
+++ OfficeLibrary/ExcelManager.cs
@@ -1,69 +1,273 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using OfficeLibrary.Extensions;
+using OfficeLibrary.Primitives;
 using OfficeOpenXml;
 
 namespace OfficeLibrary {
 	/// <summary>
 	/// Excel 管理クラス
 	/// </summary>
-	public class ExcelManager {
+	public class ExcelManager : ExcelManagerBase {
+		#region フィールド
+
+		private string _sheetName;
+		private int _position;
+
+		#endregion
+
 		#region コンストラクタ
 
 		/// <summary>
-		/// コンストラクタ
+		/// ファイルパスを指定して、
+		/// 新しいオブジェクトのインスタンスを生成します。
 		/// </summary>
 		/// <param name="path">ファイルパス</param>
-		public ExcelManager(string path)
-			: this(new FileInfo(path)) {
+		public ExcelManager(string path) : this(path, 1) {
 		}
 
 		/// <summary>
-		/// コンストラクタ
+		/// ファイルパスとシート名を指定して、
+		/// 新しいオブジェクトのインスタンスを生成します。
 		/// </summary>
-		/// <param name="file">ファイル情報</param>
-		public ExcelManager(FileInfo file) {
-			this.File = file;
+		/// <param name="path">ファイルパス</param>
+		/// <param name="sheetName">参照するシート名</param>
+		public ExcelManager(string path, string sheetName) : base(path) {
+			if (!this.File.Exists) {
+				this.AddSheet(sheetName);
+			}
+
+			this.SheetName = sheetName;
 		}
+
+		/// <summary>
+		/// ファイルパスとシート番号を指定して、
+		/// 新しいオブジェクトのインスタンスを生成します。
+		/// </summary>
+		/// <param name="path">ファイルパス</param>
+		/// <param name="position">参照するシート番号</param>
+		public ExcelManager(string path, int position) : base(path) {
+			if (!this.File.Exists) {
+				this.AddSheet();
+				this.Position = 1;
+				return;
+			}
+
+			this.Position = position;
+		}
+
+		/// <summary>
+		/// テンプレートファイルを指定して、
+		/// 新しいオブジェクトのインスタンスを生成します。
+		/// </summary>
+		/// <param name="path">ファイルパス</param>
+		/// <param name="template">テンプレートファイル情報</param>
+		public ExcelManager(string path, FileInfo template) : base(new FileInfo(path), template) {
+			this.Position = 1;
+		}
+
+		#endregion
+
+		#region インデクサー
+
+		/// <summary>
+		/// セルの文字列を取得します。
+		/// </summary>
+		/// <param name="Address">セルの位置を表す文字列</param>
+		/// <returns>セルの文字列を返します。</returns>
+		public string this[string Address] => this.FetchValueFromSheet(sheet => {
+			using (var cell = sheet.Cells[Address]) {
+				return cell?.Value?.ToString();
+			}
+		});
+
+		/// <summary>
+		/// 行、列を指定して
+		/// セルの文字列を取得します。
+		/// </summary>
+		/// <param name="Row">行</param>
+		/// <param name="Col">列</param>
+		/// <returns>セルの文字列を返します。</returns>
+		public string this[int Row, int Col] => this.FetchValueFromSheet(sheet => {
+			using (var cell = sheet.Cells[Row, Col]) {
+				return cell?.Value?.ToString();
+			}
+		});
 
 		#endregion
 
 		#region プロパティ
 
 		/// <summary>
-		/// ファイル情報
+		/// 現在参照中のシート名を取得、設定します。
 		/// </summary>
-		public FileInfo File { get; protected set; }
+		public string SheetName {
+			get => this._sheetName;
+			set {
+				try {
+					this.ReferSheet(value, sheet => this._position = sheet.Index);
+					this._sheetName = value;
+				} catch (NullReferenceException ex) {
+					throw new ArgumentException($"指定された名前のシートは存在しません。:{value}", nameof(value), ex);
+				}
+			}
+		}
+
+		/// <summary>
+		/// 現在参照中のシート番号を取得、設定します。
+		/// </summary>
+		public int Position {
+			get => this._position;
+			set {
+				try {
+					this.ReferSheet(value, sheet => this._sheetName = sheet.Name);
+					this._position = value;
+				} catch (IndexOutOfRangeException ex) {
+					throw value < 1
+						? new ArgumentException($"シート番号は 1 以上の値をしてしてください。:{value}", nameof(value), ex)
+						: new ArgumentException($"指定された番号のシートは存在しません。:{value}", nameof(value), ex);
+				}
+			}
+		}
+
+		/// <summary>
+		/// シート数を取得します。
+		/// </summary>
+		public int SheetCount => this.Fetch(x => x.Workbook.Worksheets.Count);
+
+		/// <summary>
+		/// シート名のリストを取得します。
+		/// </summary>
+		public List<string> SheetNames => this.Fetch(x => x.Workbook.Worksheets.Select(sheet => sheet.Name).ToList());
 
 		#endregion
 
 		#region メソッド
 
-		/// <summary>
-		/// Excelのデータを参照するメソッドです。
-		/// </summary>
-		/// <param name="refer">参照したデータを加工するメソッド</param>
-		public virtual void Refer(Action<ExcelPackage> refer) {
-			using (var xlsx = new ExcelPackage(this.File)) {
-				refer?.Invoke(xlsx);
-			}
-		}
+		#region AddSheet
 
 		/// <summary>
-		/// Excelのデータから値を取り出すメソッドです。
+		/// ワークシートを追加します。
 		/// </summary>
-		/// <typeparam name="TResult">戻り値の型</typeparam>
+		/// <returns>追加したワークシートのシート番号と名前を返します。</returns>
+		public Tuple<int, string> AddSheet() => this.Fetch(x => {
+			var sheet = x.Workbook.Add();
+			x.Save();
+
+			return Tuple.Create(sheet.Index, sheet.Name);
+		});
+
+		/// <summary>
+		/// シート名を指定して、
+		/// ワークシートを追加します。
+		/// </summary>
+		/// <param name="name">シート名</param>
+		/// <returns>追加したワークシートのシート番号と名前を返します。</returns>
+		public Tuple<int, string> AddSheet(string name) => this.Fetch(x => {
+			var sheet = x.Workbook.Add(name);
+			x.Save();
+
+			return Tuple.Create(sheet.Index, sheet.Name);
+		});
+
+		#endregion
+
+		#region ReferSheet
+
+		/// <summary>
+		/// シートの内容を参照します。
+		/// </summary>
+		/// <param name="reference">参照するメソッド</param>
+		public void ReferSheet(Action<ExcelWorksheet> reference)
+			=> this.ReferSheet(this.Position, reference);
+
+		/// <summary>
+		/// シートの内容を参照します。
+		/// </summary>
+		/// <param name="sheetName">シート名</param>
+		/// <param name="reference">参照するメソッド</param>
+		public void ReferSheet(string sheetName, Action<ExcelWorksheet> reference)
+			=> base.Refer(x => reference?.Invoke(x.Workbook.Worksheets[sheetName]));
+
+		/// <summary>
+		/// シートの内容を参照します。
+		/// </summary>
+		/// <param name="position">シート番号</param>
+		/// <param name="reference">参照するメソッド</param>
+		public void ReferSheet(int position, Action<ExcelWorksheet> reference)
+			=> base.Refer(x => reference?.Invoke(x.Workbook.Worksheets[position]));
+
+		#endregion
+
+		#region FetchDataFromSheet
+
+		/// <summary>
+		/// ワークシートから値を取り出します。
+		/// </summary>
+		/// <typeparam name="TResult">取り出す値の型</typeparam>
 		/// <param name="fetch">値を取り出すメソッド</param>
-		/// <returns>取り出した値を返します。</returns>
-		public virtual TResult Fetch<TResult>(Func<ExcelPackage, TResult> fetch) {
-			if (fetch == null) {
-				return default(TResult);
-			}
+		/// <returns>抽出した値を返します。</returns>
+		public TResult FetchValueFromSheet<TResult>(Func<ExcelWorksheet, TResult> fetch)
+			=> FetchValueFromSheet(this.Position, fetch);
 
-			var file = this.File;
-			using (var xlsx = new ExcelPackage(this.File)) {
-				return fetch(xlsx);
+		/// <summary>
+		/// ワークシートから値を取り出します。
+		/// </summary>
+		/// <typeparam name="TResult">取り出す値の型</typeparam>
+		/// <param name="sheetName">シート名</param>
+		/// <param name="fetch">値を取り出すメソッド</param>
+		/// <returns>抽出した値を返します。</returns>
+		public TResult FetchValueFromSheet<TResult>(string sheetName, Func<ExcelWorksheet, TResult> fetch)
+			=> base.Fetch(x => fetch(x.Workbook.Worksheets[sheetName]));
+
+		/// <summary>
+		/// ワークシートから値を取り出します。
+		/// </summary>
+		/// <typeparam name="TResult">取り出す値の型</typeparam>
+		/// <param name="position">シート番号</param>
+		/// <param name="fetch">値を取り出すメソッド</param>
+		/// <returns>抽出した値を返します。</returns>
+		public TResult FetchValueFromSheet<TResult>(int position, Func<ExcelWorksheet, TResult> fetch)
+			=> base.Fetch(x => fetch(x.Workbook.Worksheets[position]));
+
+		#endregion
+
+		#region UpdateSheet
+
+		/// <summary>
+		/// シートの内容を更新します。
+		/// </summary>
+		/// <param name="update">更新するメソッド</param>
+		public void UpdateSheet(Action<ExcelWorksheet> update)
+		=> this.UpdateSheet(this.Position, update);
+
+		/// <summary>
+		/// シートの内容を更新します。
+		/// </summary>
+		/// <param name="sheetName">シート名</param>
+		/// <param name="update">更新するメソッド</param>
+		public void UpdateSheet(string sheetName, Action<ExcelWorksheet> update) => base.Refer(x => {
+			update?.Invoke(x.Workbook.Worksheets[sheetName]);
+			if (update != null) {
+				x.Save();
 			}
-		}
+		});
+
+		/// <summary>
+		/// シートの内容を更新します。
+		/// </summary>
+		/// <param name="position">シート番号</param>
+		/// <param name="update">更新するメソッド</param>
+		public void UpdateSheet(int position, Action<ExcelWorksheet> update) => base.Refer(x => {
+			update?.Invoke(x.Workbook.Worksheets[position]);
+			if (update != null) {
+				x.Save();
+			}
+		});
+
+		#endregion
 
 		#endregion
 	}
---
+++ UnitTestExtensions/UnitTestOffice.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using ExtensionsLibrary.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeLibrary;
+using OfficeOpenXml.Style;
+
+namespace UnitTestExtensions {
+	[TestClass]
+	public partial class UnitTestOffice {
+		#region メソッド
+
+		[TestMethod]
+		[Owner(nameof(ExcelManager))]
+		[WorkItem(1)]
+		[TestCategory("コンストラクタ")]
+		public void ExcelManagerTest() {
+			var xlsx = new ExcelManager(@"C:\Excel\test.xlsx");
+
+			Assert.AreEqual("Sheet1", xlsx.SheetName);
+			Assert.AreEqual(1, xlsx.Position);
+		}
+
+		[TestMethod]
+		[Owner(nameof(ExcelManager))]
+		[TestCategory("コンストラクタ")]
+		[WorkItem(2)]
+		[ExpectedException(typeof(System.ArgumentException))]
+		public void ExcelManagerTest1() {
+			var xlsx = new ExcelManager(@"C:\Excel\test.xlsx", "XXXXX");
+		}
+
+		[TestMethod]
+		[Owner(nameof(ExcelManager))]
+		[TestCategory("コンストラクタ")]
+		[WorkItem(3)]
+		[ExpectedException(typeof(System.ArgumentException))]
+		public void ExcelManagerTest2() {
+			var xlsx = new ExcelManager(@"C:\Excel\test.xlsx", -1);
+		}
+
+		[TestMethod]
+		[Owner(nameof(ExcelManager))]
+		[TestCategory("コンストラクタ")]
+		[WorkItem(4)]
+		[ExpectedException(typeof(System.ArgumentException))]
+		public void ExcelManagerTest3() {
+			var xlsx = new ExcelManager(@"C:\Excel\test.xlsx", 99);
+		}
+
+		[TestMethod]
+		[Owner(nameof(ExcelManager))]
+		[TestCategory("コンストラクタ")]
+		[WorkItem(5)]
+		public void ExcelManagerTest4() {
+			var xlsx = new ExcelManager(@"C:\Excel\test02.xlsx", new FileInfo(@"C:\Excel\test01.xlsx"));
+		}
+
+		[TestMethod]
+		[Owner(nameof(ExcelManager))]
+		[TestCategory("追加")]
+		[WorkItem(5)]
+		public void ワークシート追加() {
+			var xlsx = new ExcelManager(@"C:\Excel\test.xlsx");
+			var cnt = xlsx.SheetCount;
+
+			var sheet = xlsx.AddSheet();
+
+			cnt++;
+			Assert.AreEqual(cnt, xlsx.SheetCount);
+		}
+
+		[TestMethod]
+		[Owner(nameof(ExcelManager))]
+		[TestCategory("追加")]
+		[WorkItem(5)]
+		public void ワークシート追加名前指定() {
+			var xlsx = new ExcelManager(@"C:\Excel\Book1.xlsx", "シート１");
+			var cnt = xlsx.SheetCount;
+
+			var sheet = xlsx.AddSheet("シート２");
+
+			cnt++;
+			Assert.AreEqual(cnt, xlsx.SheetCount);
+		}
+
+		[TestMethod]
+		[Owner("Excel 出力")]
+		[TestCategory("更新処理")]
+		public void セル更新() {
+			var xlsx = new ExcelManager(@"C:\Excel\test.xlsx");
+			xlsx.Position = xlsx.SheetCount;
+			xlsx.UpdateSheet(sheet => {
+				sheet.Cells["A1"].Value = "ネタ";
+				sheet.Cells["B1"].Value = "単価";
+				sheet.Cells["C1"].Value = "個数";
+				sheet.Cells["D1"].Value = "小計";
+
+				var rnd = new Random();
+				var neta = new[] { "鮪", "烏賊", "エンガワ", "雲丹", "イクラ", "鰺", "穴子", "石鯛", "鰯", "鰹" };
+
+				var ls = neta.Select((n, i) => new {
+					row = i + 2,
+					ネタ = n,
+					単価 = rnd.Next(100, 200),
+					個数 = rnd.Next(0, 20),
+				}).Select(r => new {
+					r.row,
+					r.ネタ,
+					r.単価,
+					r.個数,
+					小計 = $"B{r.row}*C{r.row}",
+				});
+
+				ls.ForEach((r, i) => {
+					sheet.Cells[r.row, 1].Value = r.ネタ;
+					sheet.Cells[r.row, 2].Value = r.個数;
+					sheet.Cells[r.row, 3].Value = r.単価;
+					sheet.Cells[r.row, 4].Formula = r.小計;
+				});
+
+				using (var range = sheet.Cells[1, 1, 1, 4]) {
+					// フォントをBoldにする。Italic や Strike（取消）、UnderLine も同様
+					range.Style.Font.Bold = true;
+
+					// セルの背景色指定（要 PatternType の指定）
+					range.Style.Fill.PatternType = ExcelFillStyle.Solid;
+					range.Style.Fill.BackgroundColor.SetColor(Color.DimGray);
+
+					// フォントカラーの設定
+					range.Style.Font.Color.SetColor(Color.WhiteSmoke);
+
+					// フォントサイズの指定
+					range.Style.Font.Size = 9;
+
+					// 中央揃え
+					range.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+				}
+
+				using (var range = sheet.Cells[2, 1, 11, 1]) {
+					// フォントの指定 range.Style.Font.Name でフォントだけ指定することも可
+					range.Style.Font.SetFromFont(new Font("ＭＳ Ｐ明朝", 9, FontStyle.Regular));
+				}
+
+				using (var range = sheet.Cells[2, 2, 11, 4]) {
+					range.Style.Font.SetFromFont(new Font("Calibri Light", 9, FontStyle.Regular));
+
+					// 数値を3桁区切り
+					range.Style.Numberformat.Format = "#,##0";
+				}
+			});
+		}
+
+		[TestMethod]
+		public void 更新データ確認() {
+			var xlsx = new ExcelManager(@"C:\Excel\test.xlsx");
+			xlsx.Position = xlsx.SheetCount;
+			var intRow = 1;
+			{
+				var expected = "ネタ";
+				var actual = xlsx[$"A{intRow}"];
+				Assert.AreEqual(expected, actual);
+			}
+			{
+				var expected = "単価";
+				var actual = xlsx[$"B{intRow}"];
+				Assert.AreEqual(expected, actual);
+			}
+			{
+				var expected = "個数";
+				var actual = xlsx[$"C{intRow}"];
+				Assert.AreEqual(expected, actual);
+			}
+			{
+				var expected = "小計";
+				var actual = xlsx[$"D{intRow}"];
+				Assert.AreEqual(expected, actual);
+			}
+		}
+
+		#endregion
+	}
+}
