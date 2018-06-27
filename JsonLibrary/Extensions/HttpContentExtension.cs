ff38419896f4148677929d4e7ddd8c23dc53bea5
NVTOKYO\ishikawam <ishikawam@nextvision.co.jp>
2018-06-27 15:49:38 +0900

    HttpContent 拡張メソッド追加

---
+++ JsonLibrary/Extensions/HttpContentExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace JsonLibrary.Extensions {
+	/// <summary>
+	/// HttpContent を拡張するメソッドを提供します。
+	/// </summary>
+	public static partial class HttpContentExtension {
+		#region メソッド
+
+		/// <summary>
+		/// HTTP コンテンツを JToken に変換します。
+		/// </summary>
+		/// <param name="this">HttpContent</param>
+		/// <returns>変換した JToken を返します。</returns>
+		public static async Task<JToken> ToJTokenAsync(this HttpContent @this) {
+			var contents = await @this.ReadAsStringAsync();
+			var token = contents.Deserialize<JToken>();
+			return token;
+		}
+
+		#endregion
+	}
+}
