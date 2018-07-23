606fa399ed16cd9c981a86e56a7cb3bc0b07cadb
NVTOKYO\ishikawam <ishikawam@nextvision.co.jp>
2018-07-23 22:04:40 +0900

    1.8.7.19 列挙体変換処理を修正

--- ExtensionsLibrary/Extensions/EnumExtension.cs
+++ ExtensionsLibrary/Extensions/EnumExtension.cs
@@ -11,6 +11,8 @@ namespace ExtensionsLibrary.Extensions {
 
 		#region 列挙型変換
 
+		#region ToEnum
+
 		/// <summary>
 		/// 文字列を列挙型に変換します。
 		/// </summary>
@@ -19,37 +21,43 @@ namespace ExtensionsLibrary.Extensions {
 		/// <param name="defaultValue">デフォルト値</param>
 		/// <returns>列挙型を返します。</returns>
 		public static TResult ToEnum<TResult>(this string @this, TResult? defaultValue = null) where TResult : struct {
-			if (@this.IsEmpty()) {
+			try {
+				if (@this.IsEmpty()) {
+					throw new ArgumentException("指定された文字列が null または Empty です。", nameof(@this));
+				}
+
+				return (TResult)Enum.Parse(typeof(TResult), @this);
+			} catch (Exception ex) {
 				if (!defaultValue.HasValue) {
-					throw new ArgumentNullException("this", "指定された文字列が null または Empty です。");
+					throw ex;
 				}
 
 				return defaultValue.Value;
 			}
-
-			return (TResult)Enum.Parse(typeof(TResult), @this);
 		}
 
 		/// <summary>
-		/// 数値(int)を列挙型に変換します。
+		/// 文字列を列挙型に変換します。
 		/// </summary>
 		/// <typeparam name="TResult">列挙型</typeparam>
-		/// <param name="this">数値(int)</param>
+		/// <param name="this">文字列</param>
+		/// <param name="ignoreCase">大文字と小文字を区別しない場合は true。大文字と小文字を区別する場合は false。</param>
 		/// <param name="defaultValue">デフォルト値</param>
 		/// <returns>列挙型を返します。</returns>
-		public static TResult ToEnum<TResult>(this int @this, TResult? defaultValue = null) where TResult : struct {
-			return @this.ToString().ToEnum<TResult>();
-		}
+		public static TResult ToEnum<TResult>(this string @this, bool ignoreCase, TResult? defaultValue = null) where TResult : struct {
+			try {
+				if (@this.IsEmpty()) {
+					throw new ArgumentException("指定された文字列が null または Empty です。", nameof(@this));
+				}
 
-		/// <summary>
-		/// 数値(short)を列挙型に変換します。
-		/// </summary>
-		/// <typeparam name="TResult">列挙型</typeparam>
-		/// <param name="this">数値(short)</param>
-		/// <param name="defaultValue">デフォルト値</param>
-		/// <returns>列挙型を返します。</returns>
-		public static TResult ToEnum<TResult>(this short @this, TResult? defaultValue = null) where TResult : struct {
-			return @this.ToString().ToEnum<TResult>();
+				return (TResult)Enum.Parse(typeof(TResult), @this, ignoreCase);
+			} catch (Exception ex) {
+				if (!defaultValue.HasValue) {
+					throw ex;
+				}
+
+				return defaultValue.Value;
+			}
 		}
 
 		/// <summary>
@@ -57,11 +65,69 @@ namespace ExtensionsLibrary.Extensions {
 		/// </summary>
 		/// <typeparam name="TResult">列挙型</typeparam>
 		/// <param name="this">数値(byte)</param>
-		/// <param name="defaultValue">デフォルト値</param>
 		/// <returns>列挙型を返します。</returns>
-		public static TResult ToEnum<TResult>(this byte @this, TResult? defaultValue = null) where TResult : struct {
-			return @this.ToString().ToEnum<TResult>();
-		}
+		public static TResult ToEnum<TResult>(this byte @this) where TResult : struct
+			=> (TResult)Enum.ToObject(typeof(TResult), @this);
+
+		/// <summary>
+		/// 数値(short)を列挙型に変換します。
+		/// </summary>
+		/// <typeparam name="TResult">列挙型</typeparam>
+		/// <param name="this">数値(short)</param>
+		/// <returns>列挙型を返します。</returns>
+		public static TResult ToEnum<TResult>(this short @this) where TResult : struct
+			=> (TResult)Enum.ToObject(typeof(TResult), @this);
+
+		/// <summary>
+		/// 数値(int)を列挙型に変換します。
+		/// </summary>
+		/// <typeparam name="TResult">列挙型</typeparam>
+		/// <param name="this">数値(int)</param>
+		/// <returns>列挙型を返します。</returns>
+		public static TResult ToEnum<TResult>(this int @this) where TResult : struct
+			=> (TResult)Enum.ToObject(typeof(TResult), @this);
+
+		/// <summary>
+		/// 数値(long)を列挙型に変換します。
+		/// </summary>
+		/// <typeparam name="TResult">列挙型</typeparam>
+		/// <param name="this">数値(long)</param>
+		/// <returns>列挙型を返します。</returns>
+		public static TResult ToEnum<TResult>(this long @this) where TResult : struct
+			=> (TResult)Enum.ToObject(typeof(TResult), @this);
+
+		#endregion
+
+		#region 数値変換
+
+		/// <summary>
+		/// 16 ビット符号付き整数に変換します。
+		/// </summary>
+		/// <typeparam name="TEnum">列挙体の型</typeparam>
+		/// <param name="this">列挙体</param>
+		/// <returns>16 ビット符号付き整数を返します。値が null の場合は 0 を返します。</returns>
+		public static short ToInt16<TEnum>(this TEnum @this) where TEnum : struct
+			=> Convert.ToInt16(@this);
+
+		/// <summary>
+		/// 32 ビット符号付き整数に変換します。
+		/// </summary>
+		/// <typeparam name="TEnum">列挙体の型</typeparam>
+		/// <param name="this">列挙体</param>
+		/// <returns>32 ビット符号付き整数を返します。値が null の場合は 0 を返します。</returns>
+		public static int ToInt32<TEnum>(this TEnum @this) where TEnum : struct
+			=> Convert.ToInt32(@this);
+
+		/// <summary>
+		/// 64 ビット符号付き整数に変換します。
+		/// </summary>
+		/// <typeparam name="TEnum">列挙体の型</typeparam>
+		/// <param name="this">列挙体</param>
+		/// <returns>64 ビット符号付き整数を返します。値が null の場合は 0 を返します。</returns>
+		public static long ToInt64<TEnum>(this TEnum @this) where TEnum : struct
+			=> Convert.ToInt64(@this);
+
+		#endregion
 
 		/// <summary>
 		/// Dictionary に変換します。
@@ -70,7 +136,7 @@ namespace ExtensionsLibrary.Extensions {
 		/// <returns>Dictionary を返します。</returns>
 		public static Dictionary<TEnum, int> ToDictionary<TEnum>() where TEnum : struct {
 			var ls = GetEnumValues<TEnum>();
-			return ls.ToDictionary(f => f, f => Convert.ToInt32(f));
+			return ls.ToDictionary(f => f, f => f.ToInt32());
 		}
 
 		/// <summary>
@@ -78,9 +144,8 @@ namespace ExtensionsLibrary.Extensions {
 		/// </summary>
 		/// <typeparam name="TEnum">列挙体の型</typeparam>
 		/// <returns>定数の値を格納する配列を取得します。</returns>
-		public static IEnumerable<TEnum> GetEnumValues<TEnum>() where TEnum : struct {
-			return Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
-		}
+		public static IEnumerable<TEnum> GetEnumValues<TEnum>() where TEnum : struct
+			=> Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
 
 		#endregion
 
--- UnitTestExtensions/UnitTestCommonFeatures.cs
+++ UnitTestExtensions/UnitTestCommonFeatures.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Text;
 using CommonFeaturesLibrary;
 using CommonFeaturesLibrary.Extensions;
+using CommonFeaturesLibrary.Providers.Excel;
 using ExtensionsLibrary.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.CompilerServices;
+using System.Data;
 
 namespace UnitTestExtensions {
 	[TestClass]
@@ -47,13 +50,63 @@ namespace UnitTestExtensions {
 		[DeploymentItem(@"Resources")]
 		public void CSV読込() {
 			var dir = Resources.FullName;
-			var file = new FileInfo(Path.Combine(dir, @"UserInfos.csv") );
+			var file = new FileInfo(Path.Combine(dir, @"UserInfos.csv"));
 			var tbl = file.GetCsvTable();
 			var rows = tbl.GetRows().ToList();
 
 			var expected = $"斎藤";
 			var actual = rows[0][0];
 			Assert.AreEqual(expected, actual);
+		}
+
+		#endregion
+
+		#region Excel
+
+		[TestMethod]
+		[Owner(nameof(CommonFeaturesLibrary))]
+		[TestCategory(nameof(CommonFeaturesLibrary.Providers.Excel.ExcelConnection))]
+		public void Excel読込() {
+			var file = new FileInfo(@"C:\Excel\交番カレンダー\マスタ_交番カレンダー_テンプレート.xlsx");
+			{
+				var con = new ExcelConnection(file) { TableName = "月次勤怠情報" };
+				var tbl = con.Load();
+				var rows = (
+					from row in tbl.GetRows()
+					select new {
+						月 = row.Field<double>("月"),
+						月次計画労働時間 = new TimeSpan(Convert.ToInt32(row.Field<double>("月次計画労働時間")), 0, 0),
+						月次所定時間 = new TimeSpan(Convert.ToInt32(row.Field<double>("月次所定時間")), 0, 0),
+						月次公休日数 = row.Field<double>("月次公休日数"),
+						社員区分 = row.Field<string>("社員区分"),
+					}
+				).ToList();
+
+				var expected = 4.0;
+				var actual = rows[0].月;
+				Assert.AreEqual(expected, actual);
+			}
+			{
+				var con = new ExcelConnection(file) { TableName = "日次勤怠情報" };
+				var tbl = con.Load();
+				var rows = (
+					from row in tbl.GetRows()
+					select new {
+						勤務日 = row.Field<DateTime>("勤務日"),
+						勤怠区分 = row.Field<string>("勤怠区分"),
+						所定時間 = row.Field<string>("所定時間").ToTimeSpan(),
+						出勤時間 = row.Field<string>("出勤時間").ToTimeSpan(),
+						退勤時間 = row.Field<string>("退勤時間").ToTimeSpan(),
+						休憩時間 = row.Field<string>("休憩時間").ToTimeSpan(),
+						社員区分 = row.Field<string>("社員区分"),
+						計画時間 = row.Field<string>("計画時間").ToTimeSpan(),
+					}
+				).ToList();
+
+				var expected = new DateTime(2017, 3, 16);
+				var actual = rows[0].勤務日;
+				Assert.AreEqual(expected, actual);
+			}
 		}
 
 		#endregion
