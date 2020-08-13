956dad7b16d7573ab443dd7b3553ab06c405c5d0
Makoto Ishikawa <makott0211@gmail.com>
2020-08-13 18:58:39 +0900

    - ChangeType メソッドを移動、戻り値変更

---
+++ ExtensionsLibrary/Extensions/ConvertibleExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionsLibrary.Extensions {
+	/// <summary>
+	/// <see cref="IConvertible"/> インターフェイス実装したクラスを拡張するメソッドを提供します。
+	/// </summary>
+	public static partial class ConvertibleExtension {
+		#region メソッド
+
+		#region ChangeType
+
+		/// <summary>
+		/// 指定されたオブジェクトと等しい値を持つ、指定された型のオブジェクトを返します。
+		/// </summary>
+		/// <typeparam name="TConvertible">IConvertible 型</typeparam>
+		/// <param name="this">IConvertible インターフェイスを実装するオブジェクト。</param>
+		/// <param name="conversionType">返すオブジェクトの型。</param>
+		/// <returns>型が conversionType であり、@this と等価の値を持つオブジェクト。</returns>
+		public static object ChangeType<TConvertible>(this TConvertible @this, Type conversionType)
+			where TConvertible : IConvertible
+			=> conversionType.IsNullable()
+				? (@this == null)
+					? null
+					: Convert.ChangeType(@this, conversionType.GenericTypeArguments.First())
+				: Convert.ChangeType(@this, conversionType);
+
+		#endregion
+
+		#endregion
+	}
+}
---
+++ ExtensionsLibraryTests/Extensions/ConvertibleExtensionTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionsLibrary.Extensions.Tests {
+	[TestClass]
+	public class ConvertibleExtensionTests {
+		[TestMethod]
+		[Owner(nameof(ExtensionsLibrary))]
+		[TestCategory(nameof(ConvertibleExtension))]
+		[TestCategory(nameof(ConvertibleExtension.ChangeType))]
+		public void ChangeTypeTest() {
+			Assert.AreEqual(1, "1".ChangeType(typeof(int)));
+			Assert.AreEqual(1.0, "1.0".ChangeType(typeof(double)));
+			Assert.AreEqual(1.0m, "1.0".ChangeType(typeof(decimal)));
+
+			Assert.AreEqual(1, "1".ChangeType(typeof(int?)));
+			Assert.AreEqual(1.0, "1.0".ChangeType(typeof(double?)));
+			Assert.AreEqual(1.0m, "1.0".ChangeType(typeof(decimal?)));
+
+			Assert.AreEqual(new DateTime(2020, 08, 13), "2020/08/13 00:00:00".ChangeType(typeof(DateTime?)));
+			Assert.AreEqual(new DateTime(2020, 08, 13, 12, 30, 0), "2020-8-13T12:30:00".ChangeType(typeof(DateTime?)));
+
+			var today = DateTime.Today;
+			Debug.WriteLine(today);
+			Assert.AreEqual(new DateTime(today.Year, today.Month, today.Day, 23, 59, 59), "23:59:59".ChangeType(typeof(DateTime)));
+
+			Assert.IsNull(((string)null).ChangeType(typeof(int?)));
+			Assert.IsNull(((string)null).ChangeType(typeof(double?)));
+			Assert.IsNull(((string)null).ChangeType(typeof(decimal?)));
+			Assert.IsNull(((string)null).ChangeType(typeof(DateTime?)));
+		}
+
+		[TestMethod]
+		[Owner(nameof(ExtensionsLibrary))]
+		[TestCategory(nameof(ConvertibleExtension))]
+		[TestCategory(nameof(ConvertibleExtension.ChangeType))]
+		[TestCategory(nameof(Int32) + " 型変換 例外")]
+		[ExpectedException(typeof(FormatException))]
+		public void ChangeTypeTest_Int32_FormatException() {
+			_ = "1.0".ChangeType(typeof(int));
+		}
+
+		[TestMethod]
+		[Owner(nameof(ExtensionsLibrary))]
+		[TestCategory(nameof(ConvertibleExtension))]
+		[TestCategory(nameof(ConvertibleExtension.ChangeType))]
+		[TestCategory(nameof(Int32) + " 型変換 例外")]
+		[ExpectedException(typeof(InvalidCastException))]
+		public void ChangeTypeTest_Int32_InvalidCastException() {
+			_ = ((string)null).ChangeType(typeof(int));
+		}
+
+		[TestMethod]
+		[Owner(nameof(ExtensionsLibrary))]
+		[TestCategory(nameof(ConvertibleExtension))]
+		[TestCategory(nameof(ConvertibleExtension.ChangeType))]
+		[TestCategory(nameof(Int32) + " 型変換 例外")]
+		[ExpectedException(typeof(FormatException))]
+		public void ChangeTypeTest_DateTime_FormatException() {
+			_ = "20200813".ChangeType(typeof(DateTime));
+		}
+	}
+}
